8248eb98e6e0ac8b99d1556871ba69f5ae7fb042
Jewel Crystel Magcawas <wzt206@usask.ca>
2024-07-31 14:48:01 +0000

    fix: error handling when it comes to getting input for chars or strings.

--- element.sh
+++ element.sh
@@ -8,7 +8,13 @@ get_element_details() {
   if [[ -z $1 ]]; then
     echo "Please provide an element as an argument."
   else
-    ELEMENT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.atomic_number = $1 OR e.symbol = '$1' OR e.name = '$1'")
+    # Check if input is a number or a string
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+      ELEMENT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.atomic_number = $1")
+    else
+      ELEMENT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p ON e.atomic_number = p.atomic_number JOIN types t ON p.type_id = t.type_id WHERE e.symbol = '$1' OR e.name = '$1'")
+    fi
+    
     if [[ -z $ELEMENT ]]; then
       echo "I could not find that element in the database."
     else
